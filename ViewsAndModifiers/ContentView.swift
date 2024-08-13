d1f53b4e95e189e17e9b44f00506f2c4fc94486d
abduladhil227 <abduladhil006@gmail.com>
2024-08-13 23:04:45 +0530

    View as properties and send multiple views, conditiona and environmentl modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var useRedText = false
+    
+    //View as properties
+    
+    //1.computed properties
+    //but swift will not apply the view builder properties automatically
+    //View builder - This has the effect of silently wrapping multiple views in one of those TupleView containers, so that even though it looks like we’re sending back multiple views they get combined into one TupleView.
+    
+    var text: some View{
+        Text("Hi, how are you")
+    }
+    
+   //2.stored properties
+    let text1 = Text("First")
+    let text2 = Text("Second")
+    
+    
+   //if you want to sent multiple view there are three ways
+   //1.V or H stack 2.Group 3.@ViewBuilder
+   //@ViewBuilder var spells
+    var spells: some View{
+        VStack{
+            Text("healing")
+            Text("rage")
         }
-        .padding()
+     //Group{}
+    }
+    
+    var body: some View {
+        VStack{
+            text1
+            text2
+                .foregroundStyle(.red)
+        }
+        
+        //Conditional modifier
+        Button("Hello World!"){
+            useRedText.toggle()
+        }
+        .foregroundStyle(useRedText  ? .red : .blue)
+        
+        //Environment modifier
+        //if child modifier override the same modifier the child version takes priority
+        VStack{
+            Text("Hello")
+                .font(.largeTitle)
+            Text("Abdul")
+            Text("Adhil")
+            Text("Welcome")
+        }
+        //Environment modifier
+        .font(.title)
+        
+        
     }
 }
 
