37791a92006291ca4ee0587e974a60578f80207a
abduladhil227 <abduladhil006@gmail.com>
2024-08-13 23:28:11 +0530

    New custom view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,13 +7,39 @@
 
 import SwiftUI
 
+//New custom view for text because the text views are identical apart from the text being shown in them
+
+struct capsuleText: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 10){
+            capsuleText(text: "First")
+                .foregroundColor(.white)
+            capsuleText(text: "Second")
+                .foregroundStyle(.yellow)
+            
+//            Text("First")
+//                .font(.largeTitle)
+//                .padding()
+//                .background(.blue)
+//                .clipShape(.capsule)
+//            
+//            Text("Second")
+//                .font(.largeTitle)
+//                .padding()
+//                .background(.blue)
+//                .clipShape(.capsule)
         }
         .padding()
     }
