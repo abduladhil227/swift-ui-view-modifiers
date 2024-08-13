756c2c243618245f16c67df78102b99aa45b77de
abduladhil227 <abduladhil006@gmail.com>
2024-08-13 23:52:11 +0530

    custom modifiers and extension View protocal with custom modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+struct Title: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 20))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View{
+        modifier(Title())
+    }
+}
+
+// custom view modifiers can have their own stored properties, whereas extensions to View cannot.
+
+struct WaterMark: ViewModifier{
+    var text: String// stored propertie
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing){
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View{
+    func watermarked(with text: String) -> some View{
+        modifier(WaterMark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Abdul Adhil")
+        
+        
+        Text("Hello, world!")
+            .titleStyle()
+//            .modifier(Title())
+        
     }
 }
 
